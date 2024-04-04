99c451d94df04ae74e125952c45c22bbc5adb655
Sanqui <me@sanqui.net>
2024-04-04 20:11:55 +0200

    pyhxcfe: Convert whole directory of dumps into specified formats

--- pyhxcfe.py
+++ pyhxcfe.py
@@ -3,40 +3,52 @@
 
 from os import mkdir
 import os
+from sys import argv
 import subprocess
 import shlex
 from pathlib import Path
+
+from tqdm import tqdm
 
 HXCFE_BINARY_PATH = '/home/sanqui/pauline/HxCFloppyEmulator_soft_beta (1)/HxCFloppyEmulator_soft/HxCFloppyEmulator_Software/Windows_x64/hxcfe.exe'
 
 
 FORMATS = [
     ('GENERIC_XML', 'xml'),
+    ('RAW_IMG', 'img'),
+    ('RAW_LOADER', 'img'),
     ('BMP_IMAGE', 'bmp'),
     ('BMP_STREAM_IMAGE', 'bmp'),
     ('BMP_DISK_IMAGE', 'bmp')
 ]
 
-disk_caputres_dir = Path('disk_captures')
+disk_caputres_dir = Path(argv[1])
+
+dirs = []
 
 for floppy_dir in disk_caputres_dir.iterdir():
     for floppy_subdir in floppy_dir.iterdir():
         if floppy_subdir.name.endswith("_parsed"):
             continue
 
-        print(f"Parsing {floppy_subdir.name}")
+        dirs.append((floppy_dir, floppy_subdir))
 
-        parsed_dir = floppy_dir / (floppy_subdir.name + "_parsed")
-        if not os.path.exists(parsed_dir):
-            mkdir(parsed_dir)
+for floppy_dir, floppy_subdir in tqdm(dirs):
+    tqdm.write(f"Parsing {floppy_subdir.name}")
 
-        first_file = next(floppy_subdir.iterdir())
-        cmd = [
-            HXCFE_BINARY_PATH,
-            '-finput:' + shlex.quote(str(first_file)),
-        ]
+    parsed_dir = floppy_dir / (floppy_subdir.name + "_parsed")
+    if not os.path.exists(parsed_dir):
+        mkdir(parsed_dir)
 
-        for fmt, extension in FORMATS:
-            cmd.append('-conv:' + fmt)
-            cmd.append('-foutput:' + shlex.quote(str(parsed_dir / (f'{fmt}.{extension}'))))
-        subprocess.run(cmd, check=True)
+    first_file = next(floppy_subdir.iterdir())
+    cmd = [
+        HXCFE_BINARY_PATH,
+        '-finput:' + shlex.quote(str(first_file)),
+    ]
+
+    for fmt, extension in FORMATS:
+        cmd.append('-conv:' + fmt)
+        cmd.append('-foutput:' + shlex.quote(str(parsed_dir / (f'{fmt}.{extension}'))))
+
+    with open(parsed_dir / 'stdout.txt', 'w') as f_stdout, open(parsed_dir / 'stderr.txt', 'w') as f_stderr:
+        subprocess.run(cmd, stdout=f_stdout, stderr=f_stderr, check=True)
