614dfddd27a99f316a2e33db5684fa61bc05d183
Sanqui <me@sanqui.net>
2024-04-05 11:20:11 +0200

    pauline.py: send stop command on KeyboardInterrupt

--- pauline.py
+++ pauline.py
@@ -32,24 +32,28 @@ async def pauline_batch(pauline_addr: str, floppy_names: list[str]):
             await send_ws(ws, "set MACINTOSH_GCR_MODE 0")
             await send_ws(ws, "index_to_dump 0")
             await send_ws(ws, "dump_time 800")
-            await send_ws(ws, f'dump {floppy_index} 0 {NUM_TRACKS} 0 1 0 0 0 0 "{filename}" "" 1 AUTO_INDEX_NAME "" "" ""')
-            bar = tqdm.tqdm(total=NUM_TRACKS, desc='track', leave=False)
-            bar.update(0)
-            while True:
-                message = await ws.recv()
-                bar.write(f"[{num_str}] <<< {message.strip()}")
-                # use regex to extract 37 and 0 from ...t_rh6791-0001/track37.0.hxcstream
-                match = re.search(r'/track(\d+)\.(\d+)\.hxcstream', message)
-                if match:
-                    #track = int(match.group(1))
-                    #side = int(match.group(2))
-                    #assert side < 2
-                    #progress = track + 0.5 * side
-                    bar.update(0.5)
-                if message.startswith('OK : Done...'):
-                    break
-            bar.close()
-            bar_outer.update(1)
+            try:
+                await send_ws(ws, f'dump {floppy_index} 0 {NUM_TRACKS} 0 1 0 0 0 0 "{filename}" "" 1 AUTO_INDEX_NAME "" "" ""')
+                bar = tqdm.tqdm(total=NUM_TRACKS, desc='track', leave=False)
+                bar.update(0)
+                while True:
+                    message = await ws.recv()
+                    bar.write(f"[{num_str}] <<< {message.strip()}")
+                    # use regex to extract 37 and 0 from ...t_rh6791-0001/track37.0.hxcstream
+                    match = re.search(r'/track(\d+)\.(\d+)\.hxcstream', message)
+                    if match:
+                        #track = int(match.group(1))
+                        #side = int(match.group(2))
+                        #assert side < 2
+                        #progress = track + 0.5 * side
+                        bar.update(0.5)
+                    if message.startswith('OK : Done...'):
+                        break
+                bar.close()
+                bar_outer.update(1)
+            except KeyboardInterrupt:
+                await send_ws(ws, 'stop')
+                raise
         
         bar_outer.close()
         print("Returning heads...")
