4334e3d9ddefe64bdbd7f798b2f0b48df0368a8e
Sanqui <me@sanqui.net>
2024-04-27 18:14:02 +0200

    Parallelize pyhxcfe.py

--- pyhxcfe.py
+++ pyhxcfe.py
@@ -7,35 +7,27 @@ from sys import argv
 import subprocess
 import shlex
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from tqdm import tqdm
 
-HXCFE_BINARY_PATH = '/home/sanqui/pauline/HxCFloppyEmulator_soft_beta (1)/HxCFloppyEmulator_soft/HxCFloppyEmulator_Software/Windows_x64/hxcfe.exe'
+HXCFE_BINARY_PATH = '../HxCFloppyEmulator_soft/HxCFloppyEmulator_Software/Windows_x64/hxcfe.exe'
 
+MAX_WORKERS=7
 
 FORMATS = [
     ('GENERIC_XML', 'xml'),
     ('RAW_IMG', 'img'),
     ('RAW_LOADER', 'img'),
-    ('BMP_IMAGE', 'bmp'),
-    ('BMP_STREAM_IMAGE', 'bmp'),
-    ('BMP_DISK_IMAGE', 'bmp')
+    ('HXC_HFE', 'hfe'),
+    ('HXC_HFEV3', 'hfe'),
+    ('HXC_EXTHFE', 'hfe'),
+    ('PNG_IMAGE', 'png'),
+    ('PNG_STREAM_IMAGE', 'png'),
+    ('PNG_DISK_IMAGE', 'png'),
 ]
 
-disk_caputres_dir = Path(argv[1])
-
-dirs = []
-
-for floppy_dir in disk_caputres_dir.iterdir():
-    for floppy_subdir in floppy_dir.iterdir():
-        if floppy_subdir.name.endswith("_parsed"):
-            continue
-
-        dirs.append((floppy_dir, floppy_subdir))
-
-for floppy_dir, floppy_subdir in tqdm(dirs):
-    tqdm.write(f"Parsing {floppy_subdir.name}")
-
+def process_directory(floppy_dir: Path, floppy_subdir: Path):
     parsed_dir = floppy_dir / (floppy_subdir.name + "_parsed")
     if not os.path.exists(parsed_dir):
         mkdir(parsed_dir)
@@ -52,3 +44,31 @@ for floppy_dir, floppy_subdir in tqdm(dirs):
 
     with open(parsed_dir / 'stdout.txt', 'w') as f_stdout, open(parsed_dir / 'stderr.txt', 'w') as f_stderr:
         subprocess.run(cmd, stdout=f_stdout, stderr=f_stderr, check=True)
+    
+    return floppy_subdir.name
+
+
+disk_caputres_dir = Path(argv[1])
+
+dirs = []
+
+for floppy_dir in disk_caputres_dir.iterdir():
+    for floppy_subdir in floppy_dir.iterdir():
+        if floppy_subdir.name.endswith("_parsed"):
+            continue
+
+        dirs.append((floppy_dir, floppy_subdir))
+
+dirs.sort()
+
+with tqdm(total=len(dirs)) as pbar:
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as ex:
+        futures = [ex.submit(process_directory, *dir) for dir in dirs]
+        for future in as_completed(futures):
+            try:
+                result = future.result()
+                pbar.write(f"Completed {result}")
+            except Exception as ex:
+                pbar.write(f"Failed to complete: {type(ex).__name__}: {ex}")
+            pbar.update(1)
+
